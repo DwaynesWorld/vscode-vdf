20043fb070bac5cf39aa2b8a4ad91b40fec83d2e
DwaynesWorld <kthompson713@gmail.com>
2018-08-26 14:53:39 -0500

    Cleanup

--- src/common/util.ts
+++ src/common/util.ts
@@ -6,11 +6,8 @@ export function runVdfIndentation(document: vscode.TextDocument) {
   if (document !== null) {
     const extensionPath = vscode.extensions.getExtension("hcss.vdfpack")
       .extensionPath;
-    let vdfSource = path.join(
-      extensionPath,
-      "resources\\indentation\\VDFSource.exe"
-    );
-    vscode.window.setStatusBarMessage(`Indenting: ${document.fileName}`, 5000);
+    let vdfSource = path.join(extensionPath, "resources\\VDFSource.exe");
+    vscode.window.setStatusBarMessage(`Indenting: ${document.fileName}`, 3000);
     execFile(vdfSource, [document.fileName]);
   }
 }
